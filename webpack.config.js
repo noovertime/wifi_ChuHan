09b02c012f30ceb3b4c4e751e7f43b7133556ccd
noovertime <g20020910@gmail.com>
2019-02-28 15:46:58 +0900

    start

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const webpack = require('webpack');
+const path = require('path');
+
+module.exports = {
+    entry: './hello.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        publicPath: '/dist/',
+        filename: 'bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                include: path.join(__dirname),
+                exclude: /(node_modules)|(dist)/, 
+		loader : "babel-loader",  
+		resolve: {
+			extensions: ['.js'] 
+		} 
+            }
+        ]
+    }
+};
